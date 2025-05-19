a465cbae0be3d28279bde11b05cebc9cdb4cff00
Ludvin Alonzo <165453332+Lud1024@users.noreply.github.com>
2025-05-19 16:57:04 -0600

    corrige

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const path = require('path')
 const saveRoutes = require('./routes/save.routes')
 const connectDB = require('./config/db')
 
+
 const app = express()
 const PORT = process.env.PORT || 5000
-const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
